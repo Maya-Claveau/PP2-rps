6699c549a73f8b33c04ccb03e8fa6b78b982c27e
Maya-Claveau <maya.claveau@gmail.com>
2021-11-16 15:43:11 +0000

    Add winner function and increment score function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,47 +6,83 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", displayRandomImage)        
-        
-        //    {} if (this.getAttribute("data-type") === "rock") {
-                                                 
-        //     } else {
-        //         let gameType = this.getAttribute("data-type");
-        //         alert(`You clicked ${gameType} button!`);
-        //     }            
-        
     }
 });
 
-
+/**
+ *  display a random image
+ */ 
 function displayRandomImage() {
-    // get all the images
+    // get all the images of rock paper and scissors
 
-    var myPix = ["1.png", "2.png", "3.png"];
+    let myPix = ["1.png", "2.png", "3.png"];
 
-    // get a random index
+    // get a random index    
 
-    
-
-    var randomIndex = Math.floor(Math.random() * myPix.length);
+    let randomIndex = Math.floor(Math.random() * myPix.length);
     
     // display the image
     document.getElementById("image-box").src = window.location.href + 'assets/images/' + myPix[randomIndex];
 }
 
+function rpsGame() {
 
-function runGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundNumber = 0;
+
+    // to be continued
 
 }
 
-function checkAnswer() {
+/**
+ *  check who wins
+ */ 
+function winner(player, computer) {
+    const playerScore = document.querySelector("#player");
+    const computerScore = document.querySelector("#pc");
+    // player = player.toLowerCase();
+    // computer = computer.toLowerCase(); not sure what this code does, need to look at them later
 
+    if (player === computer) {
+        alert(`It's a tie! Please try again!`)
+    } else if (player == "rock") {
+        if (computer == "paper") {
+            incrementsPcScore();
+        } else {
+            incrementPlayerScore();
+        }
+    } else if (player == "paper") {
+        if (computer == "rock") {
+            incrementPlayerScore();
+        } else {
+            incrementsPcScore();
+        }
+    } else if (player == "scissors") {
+        if (computer == "rock") {
+            incrementsPcScore();
+        } else {
+            incrementPlayerScore();
+        }
+    }
+
+    
 }
+winner();
 
+/**
+ *  get the current score from the dom and add 1 to the player score
+ */ 
 function incrementPlayerScore() {
-
+    let previousScore = parseInt(document.getElementById("player").innerText);
+    document.getElementById("player").innerText = ++previousScore;
 }
 
+/**
+ * get the current score from the dom and add 1 to the pc score
+ */
 function incrementsPcScore() {
-
+    let previousScore = parseInt(document.getElementById("pc").innerText);
+    document.getElementById("pc").innerText = ++previousScore;
 }
 
