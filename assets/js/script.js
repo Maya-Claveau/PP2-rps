b588c88899e76916c7bc9066b73c0e7a7bc9e150
Maya-Claveau <maya.claveau@gmail.com>
2021-11-19 17:15:43 +0000

    Add pop up for the game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,46 @@ startNewGame.addEventListener("click", function () {
     playGame();
 });
 
+/**
+ * pop up on game rule button 
+ */
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]")
+const closeModalButtons = document.querySelectorAll("[data-close-button]")
+const overlay = document.getElementById("overlay")
+
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = document.querySelector(button.dataset.modalTarget) // select the modal
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".modal")
+        closeModal(modal)
+    })
+})
+
+overlay.addEventListener("click",() => {
+    const modals = document.querySelectorAll(".modal.active")
+    modals.forEach(modal => {
+       closeModal(modal) 
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add("active")
+    overlay.classList.add("active")    
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove("active")
+    overlay.classList.remove("active")    
+}
 
 /**
  *  display a random image and computer's choice
